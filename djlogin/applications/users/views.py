831dfd362b4b3da52e9da764f4873dc257d5785d
cscthian <csantacruz1127@gmail.com>
2023-04-17 11:02:00 -0500

    commit post curso

--- djlogin/applications/users/views.py
+++ djlogin/applications/users/views.py
@@ -30,46 +30,21 @@ class GoogleLoginView(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
+        print('===========', 'post execute')
+        print(request.data)
         serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        # serializer.is_valid(raise_exception=True)
+        if serializer.is_valid():
+            print('todo muy bien')
+        else:
+            print(serializer.errors)
         #
         id_token = serializer.data.get('token_id')
         #
-        decoded_token = auth.verify_id_token(id_token)
+        print('=====', id_token)
         #
-        #
-        email = decoded_token['email']
-        name = decoded_token['name']
-        avatar = decoded_token['picture']
-        verified = decoded_token['email_verified']
-        #
-        usuario, created = User.objects.get_or_create(
-            email=email,
-            defaults={
-                'full_name': name,
-                'email': email,
-                'is_active': True
-            }
-        )
-        #
-        if created:
-            # creamos el token
-            token = Token.objects.create(user=usuario)
-        else:
-            # Creamos el nuevo usuario
-            token = Token.objects.get(user=usuario)
-        #
-        userGet = {
-            'id': usuario.pk,
-            'email': usuario.email,
-            'full_name': usuario.full_name,
-            'genero': usuario.genero,
-            'date_birth': usuario.date_birth,
-            'city': usuario.city
-        }
         return Response({
-            'token': token.key,
-            'user': userGet
+            'status': 'ok',
         })
 
 # Create your views here.
